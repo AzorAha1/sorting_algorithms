5b8fdddfd3844890a221b1c08b854ad29beb63ea
AzorAha1 <wizfaiz@icloud.com>
2023-10-21 15:51:43 +0100

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, minimum, temp;
+	size_t i, j, minimum, temp, swapped;
 
 	if (!array && size < 2)
 	{
@@ -17,6 +17,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		minimum = i;
+		swapped = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[minimum] > array[j])
@@ -27,7 +28,13 @@ void selection_sort(int *array, size_t size)
 		temp = array[i];
 		array[i] = array[minimum];
 		array[minimum] = temp;
+		swapped = 1;
 		print_array(array, size);
+		if (swapped == 0)
+		{
+			break;
+		}
 	}
+
 
 }
