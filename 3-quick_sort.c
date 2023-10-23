07b5fae46f1d3f9c6fad64ffbabbf19345fa0f09
NeneNafe <nenechiwaya@gmail.com>
2023-10-23 05:09:48 -0700

    Corrections

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,32 +5,36 @@
  * @array: The array to be sorted
  * @high: the high index
  * @low: the low index
- *
+ * @size: The size of the array
  * Return: The pivot index
  */
-size_t lom_partition(int *array, ssize_t low, ssize_t high)
+size_t lom_partition(int *array, ssize_t low, ssize_t high, size_t size)
 {
-	int pivot, temp;
-	ssize_t i = low -1, j;
+	int pivot, temp = 0;
+	ssize_t i = low - 1, j = low;
 
 	pivot = array[high];
-	for (j = low; j < high; j++)
+	for (; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-
-			print_array(array, high + 1);
+			if (array[i] != array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-
-	print_array(array, high + 1);
+	if (array[i + 1] != array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 
@@ -39,18 +43,18 @@ size_t lom_partition(int *array, ssize_t low, ssize_t high)
  * @array: the array to be sorted
  * @low: low index
  * @high: high index
+ * @size: The size of array
  */
 
-void quicksort(int *array, ssize_t low, ssize_t high)
+void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
 {
 	size_t p;
 
 	if (low < high)
 	{
-		p = lom_partition(array, low, high);
-
-		quicksort(array, low, p - 1);
-		quicksort(array, p + 1, high);
+		p = lom_partition(array, low, high, size);
+		quicksort(array, low, p - 1, size);
+		quicksort(array, p + 1, high, size);
 	}
 }
 
@@ -65,5 +69,5 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quicksort(array, 0, size - 1);
+	quicksort(array, 0, size - 1, size);
 }
